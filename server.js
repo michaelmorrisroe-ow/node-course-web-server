29829c4b6380b2f6def5e7e03b6ad0bd654b0c46
Mike Morrisroe <morrisroem@dub-mac-morris.eu.mrshmc.com>
2017-05-22 16:13:53 +0100

    set up app.js and package for Heroku compatibility

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+
+const port = process.env.PORT || 3000; //heroku or local
 let app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 
-app.use((req, res, next) => {
-    res.render('maintenance.hbs');
-});
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
 
 app.use((req, res, next) => {
     let now = new Date().toString();
@@ -51,6 +53,6 @@ app.get('/bad', (req, res) => {
     })
 });
 
-app.listen(3000), () => {
-    console.log('Server is up on port 3000')
+app.listen(port), () => {
+    console.log(`Server is up on port ${port}`)
 };
